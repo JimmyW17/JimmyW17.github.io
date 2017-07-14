158db82d34bd3330248ff45bf472a147a34f487d
Jimmy Wu <wujimmy94@gmail.com>
2017-07-14 11:36:15 -0400

    removed flexbox, navbar shrink on scroll added

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-// FlexSlider
 $(window).on('load',function() {
-  $('.flexslider').flexslider({
-    animation: "fade",
-    slideshow: false,
-    controlNav: true,
-    controlsContainer: "#navbar",
-    manualControls: ".nav_ul li",
-    animationLoop: true,
-    directionNav: true
-  });
-
-  var nav_link = document.getElementsByClassName("nav_link");
-  nav_link[0].addEventListener('click', function() {
-    window.scrollTo(0,0);
-  });
-  nav_link[1].addEventListener('click', function() {
-    window.scrollTo(0,0);
-  });
-  nav_link[2].addEventListener('click', function() {
-    window.scrollTo(0,0);
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('nav').addClass('shrink');
+    } else {
+      $('nav').removeClass('shrink');
+    }
   });
 });
